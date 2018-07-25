5bf5a55102a19221efcc426d5347d3d18ab94c4a
kamchai <kamchai2013@gmail.com>
2018-07-25 14:24:26 +0700

    FIX

--- haproxy_expire_cert.sh
+++ haproxy_expire_cert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DOMAIN=$1
 CERT_PATH={PATH_SSL}
-CERT=$(date --date="$(openssl x509 -enddate -noout -in "$CERT_PATH$DBNAME.pem" | cut -d= -f 2)" --iso-8601)
+CERT=$(date --date="$(openssl x509 -enddate -noout -in "$CERT_PATH$DOMAIN.pem" | cut -d= -f 2)" --iso-8601)
 DIFF=$(($(($(date -d "$CERT" "+%s") - $(date  "+%s"))) / 86400))
 echo $DIFF 
 
